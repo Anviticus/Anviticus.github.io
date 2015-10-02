ed75a05654f0d6f734c60f9efec52129279fa2d4
Anviticus <missionbit@missionbits-MacBook-Air-9.local>
2015-10-01 17:54:37 -0700

    added js

---
+++ fightthepower.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   $("#heading1").click(function () {
+       $("#subheading1").remove () ;
+       
+       //$("textbox").val();
+       
+   });
+    
+    $("#gobutton").click (function () {
+    $("#thepercent").val();
+      alert ("Yourright!");
+    });
+   
+    console.log("test");
+});
